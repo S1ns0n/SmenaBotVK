2ffd8c396b66ed1c6351452b717dae6a6ba14db5
Глушков Некет <operator1702@yandex.ru>
2026-03-01 23:39:58 +0900

    Добавлена проверка, проходил ли тип анкету

--- bot/handlers/anketa0.py
+++ bot/handlers/anketa0.py
@@ -8,7 +8,7 @@ from bot.keyboards import empty_kb
 from database import db_manager
 
 
-class FirstAnketaState(BaseStateGroup):
+class ZeroAnketaState(BaseStateGroup):
     FIO = "fio"
     SEX = "sex"
     AGE = "age"
@@ -17,37 +17,41 @@ class FirstAnketaState(BaseStateGroup):
 
 @labeler.message(text="анкета0")
 async def start_handler(message: Message):
+    if await db_manager.has_user_anketa(peer_id=message.peer_id, anketa_type="anketa0"):
+        await message.answer("Вы уже прошли анкету")
+        return
+
+
     await message.answer("Привет! Давай заполним анкету.\n\nРаздел 1. Персональные данные", keyboard=empty_kb)
     await message.answer("1. Укажите ваше полное ФИО")
-    await state_dispanser.set(message.peer_id, FirstAnketaState.FIO)
-    # проверка, проходил ли тип уже анкету и очистка state на всякий случай
+    await state_dispanser.set(message.peer_id, ZeroAnketaState.FIO)
 
 
-@labeler.message(state=FirstAnketaState.FIO)
+@labeler.message(state=ZeroAnketaState.FIO)
 async def fio_process(message: Message):
     fio = message.text.strip()
     ctx_storage.set("fio", fio)
     await message.answer("2. Укажите ваш пол", keyboard=sex_kb)
-    await state_dispanser.set(message.peer_id, FirstAnketaState.SEX)
+    await state_dispanser.set(message.peer_id, ZeroAnketaState.SEX)
 
 
-@labeler.message(state=FirstAnketaState.SEX)
+@labeler.message(state=ZeroAnketaState.SEX)
 async def sex_process(message: Message):
     sex = message.text.strip().lower()
     ctx_storage.set("sex", sex)
     await message.answer("3. Укажите ваш возраст", keyboard=empty_kb)
-    await state_dispanser.set(message.peer_id, FirstAnketaState.AGE)
+    await state_dispanser.set(message.peer_id, ZeroAnketaState.AGE)
 
 
-@labeler.message(state=FirstAnketaState.AGE)
+@labeler.message(state=ZeroAnketaState.AGE)
 async def age_process(message: Message):
     age = message.text.strip()
     ctx_storage.set("age", age)
     await message.answer("4. Укажите вашу школу, колледж или лицей")
-    await state_dispanser.set(message.peer_id, FirstAnketaState.SCHOOL)
+    await state_dispanser.set(message.peer_id, ZeroAnketaState.SCHOOL)
 
 
-@labeler.message(state=FirstAnketaState.SCHOOL)
+@labeler.message(state=ZeroAnketaState.SCHOOL)
 async def school_process(message: Message):
     school = message.text.strip()
     ctx_storage.set("school", school)
@@ -60,10 +64,10 @@ async def school_process(message: Message):
     }
 
     await db_manager.save_anketa(peer_id=message.peer_id,
-                                 anketa_type="anketa1",
+                                 anketa_type="anketa0",
                                  data=anketa_data)
     user_anketa = await db_manager.get_anketa_data(peer_id=message.peer_id,
-                                     anketa_type="anketa1")
+                                     anketa_type="anketa0")
 
     result = f"""✓ Анкета успешно заполнена!
 
