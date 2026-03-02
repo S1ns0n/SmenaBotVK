4bfbc965d363c6c312fc408085b331e9bfb94845
Глушков Некет <operator1702@yandex.ru>
2026-03-03 01:00:43 +0900

    анкета №3 добавлена в пул хендлеров

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from vkbottle.bot import Bot
 from bot.labeler_config import labeler, state_dispanser
 from config import Config
-from bot.handlers import anketa1_labeler, anketa2_labeler
+from bot.handlers import anketa1_labeler, anketa2_labeler, anketa3_labeler
 
 
 
 bot = Bot(token=Config.BOT_TOKEN, labeler=labeler, state_dispenser=state_dispanser)
 labeler.load(anketa1_labeler)
 labeler.load(anketa2_labeler)
+labeler.load(anketa3_labeler)
+
 
 
 if __name__ == "__main__":
