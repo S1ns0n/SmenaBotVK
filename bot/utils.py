ca75d1a208e96d046d397672a9686aa89c1fd9ed
Глушков Некет <operator1702@yandex.ru>
2026-03-04 23:54:32 +0900

    Метод рандомного выбора строки из списка строк

--- bot/utils.py
+++ bot/utils.py
@@ -1,6 +1,10 @@
 import re
+import random
 
 def remove_brackets_text(text: str) -> str:
     result = re.sub(r'\[[^\]]*\]', '', text)
     result = re.sub(r'(?<!\n)[ ]{2,}(?!\n)', ' ', result)
     return result.strip()
+
+def get_random_greeting(text_list: list):
+    return random.choice(text_list)
